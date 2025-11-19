89a2c4701d6ddbccebcb7ab96489f930355ea787
Kiran <kiran.sadara6730@gmail.com>
2025-11-19 19:54:01 +0530

    Logging in using user credentials done

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import  CredentialsProvider from "next-auth/providers/credentials";
+const handler = NextAuth({
+    providers:[
+        CredentialsProvider({
+    // The name to display on the sign in form (e.g. "Sign in with...")
+    name: "Email",
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "kiran@gmail.com" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      // Add logic here to look up the user from the credentials supplied
+        const username = credentials?.username;
+        const password = credentials?.password;
+
+        //adding the logic of the database
+       console.log(username )
+       console.log(password) 
+        const user = {
+            name:"kiran",
+            id:"1",
+            username :"kiran@gmail.com"
+        }
+
+        if(user){
+            return user;
+        }
+        else{
+            return null;
+        }
+        
+
+   
+
+
+       
+    }
+  })
+]
+    
+})
+
+export { handler as GET, handler as POST };

c33499c2e321f4834746fb95c62d40aedb3550a0
Kiran <kiran.sadara6730@gmail.com>
2025-11-19 19:20:04 +0530

    First request is SSR in nextjs so localstorage is not defined in server

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,29 +1,24 @@
 import { loadComponents } from "next/dist/server/load-components"
-import { useCallback, useEffect, useState } from "react"
-"use client"
+import axios from 'axios'
 export default async function Profile(){
 
-     const [profile,setProfile] = useState('');
-    useEffect(()=>{
-        
-        async function GetProfile(){
-
+   
+   
         
        const response:any =  await axios.get("http://localhost:3000/api/profile",{
             headers:{
                 authorization : localStorage.getItem("token")
             }
         })
-        setProfile(response.data.avatarUrl)
-    }
-
-    GetProfile();
+    const profile = response.data.avatarUrl
 
 
-    },[])
+
+
+
     return(
         <div>
-            Welcome to profile page
+            Welcome to profile page {profile}
         </div>
     )
 }
